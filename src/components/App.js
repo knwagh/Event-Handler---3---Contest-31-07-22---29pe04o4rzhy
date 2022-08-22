cc068f4d271143a03fe8adf192a1d8faff978fb5
knwagh <107362567+knwagh@users.noreply.github.com>
2022-08-22 17:01:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,20 @@ const App = () => {
 
   const handleInput = (event) =>{
    // use console.log
+    console.log(`Input in #${event.target.id} is ${event.target.value}`)
   }
 
   // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <input id="text-input" type={'text'} onChange={handleInput} />
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
+      <input id="num-input"  type={'number'} onChange={handleInput} />
       <br/>
     </div>
   )
